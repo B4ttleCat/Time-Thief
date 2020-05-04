e8c5c0b2a4ecd49482db15bd8240cf21af007333
Cam Newton <cam.newton@gmail.com>
2020-05-04 23:59:49 +1000

    Initial commit
    You love it

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10f;
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+    private Camera _cam;
+    private Vector2 _mousePos;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+
+        _mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate ()
+    {
+        _rb.MovePosition(_rb.position + _movement * _speed * Time.deltaTime);
+
+        // Vector2 lookDirection = _mousePos - _rb.position;
+        //
+        // // consider adding "- 90f" to the end here to fix potential rotational issues
+        // float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        // _rb.rotation = angle;
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float projectileForce = 20f;
+
+    private Vector2 _mousePos;
+    private Rigidbody2D _rb;
+    private Camera _cam;
+
+    void Awake()
+    {
+        _cam = Camera.main;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Vector2 lookDirection = _mousePos - _rb.position;
+        Debug.DrawLine(transform.position, _mousePos, Color.magenta, 0.5f);
+        
+        GameObject projectile = Instantiate(projectilePrefab, 
+                                            projectileSpawnPoint.position, 
+                                            quaternion.identity);
+        
+        // projectile.transform.LookAt(_mousePos);
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+        rb.AddForce(projectileSpawnPoint.right * projectileForce, ForceMode2D.Impulse);
+    }
+}
