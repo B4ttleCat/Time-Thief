f0bac2b5708eccd34f1733e2c5f6be9f4056dc57
Cam Newton <cam.newton@gmail.com>
2020-05-06 21:53:56 +1000

    Enemy following player

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Setup")]
+    [SerializeField] private GameObject _target;
+
+    [Header("Gameplay")]
+    [SerializeField] private float movementSpeed = 5f;
+
+    private Vector3 _targetPos;
+
+    private void Awake()
+    {
+    }
+
+    void Update()
+    {
+        _targetPos = _target.transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, _targetPos,
+            movementSpeed * Time.deltaTime);
+    }
+}
